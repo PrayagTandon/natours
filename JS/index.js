7cdf3c38ae681d6360c40c753254a3507f9b2fc5
Prayag Tandon <tandonprayag99@gmail.com>
2023-04-04 16:36:26 +0530

    First commit

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+var navClose = (function() {
+    
+    //Create a HTML collection of links
+    var navigationItem = document.body.getElementsByClassName('navigation__link');
+ 
+    //Convert the HTML collection to an Array
+    var navigationItemArr = Array.prototype.slice.call(navigationItem);
+    
+    //Loop through each list item to make checked styling disappear upon click
+    navigationItemArr.forEach(function(current){
+        current.addEventListener('click', function(){
+            document.getElementById('navigation__toggle').checked = false;
+        })
+    });
+    
+})();
